2df0c0e774501e12a5ba4729b8ec4fd26e0fc2db
Roman <roman.blyk@gmail.com>
2021-03-16 00:16:52 +0200

    feat: work on function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable (num) {
+    let arr = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"];
+    let arrDecimalNum = ['thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+   // if(number<=arr) {return `${arr[number]}`}
+
+    let objUnitsAndTens = {
+        "one": 'one',
+        "two": 'two',
+        "three": 'three',
+        "four": 'four',
+        "five": 'five',
+        "six": 'six',
+        "seven": 'seven',
+        "eight": 'eight',
+        "nine": 'nine',
+        "ten": 'ten',
+        "eleven": 'eleven',
+        "twelve": 'twelve',
+        "thirteen": 'thirteen',
+        "fourteen": 'fourteen',
+        "fifteen": 'fifteen',
+        "sixteen": 'sixteen',
+        "seventeen": 'seventeen',
+        "eighteen": 'eighteen',
+        "nineteen": 'nineteen',
+        "twenty": 'twenty',
+        "thirty": 'thirty',
+        "fourty": 'fourty',
+        "fifty": 'fifty',
+        "sixty": 'sixty',
+        "seventy": 'seventy',
+        "eighty": 'eighty',
+        "ninety": 'ninety',
+    };
+
+    for(let key in objUnitsAndTens) {
+        if(num == key) {
+            return objUnitsAndTens[key];
+        }
+    }
   
-}
+};
