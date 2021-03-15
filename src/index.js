2a893b985377a4ed06317dae51011e8145471be9
Roman <roman.blyk@gmail.com>
2021-03-16 01:10:40 +0200

    feat: completed function readeble

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ module.exports = function toReadable (num) {
     let arr = ['zero', "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"];
     let arrDecimalNum = ['', '', 'twenty', 'thirty','forty','fifty','sixty','seventy','eighty','ninety'];
     for(i=21;i<1000;i++) {
-        if(num<100) {
+        if(i<100) {
              arr.push(arrDecimalNum[Math.floor(i/10)] + (i%10==0?'':' '+arr[i%10]));
         }else {
-            arr.push(arr[Math.floor(i/100)] +' hundred'+(i%100==0?'':' '+arr[i%100]));
+            arr.push(arr[Math.floor(i/100)] + " hundred" + (i%100 == 0 ? "" : " "+arr[i%100]));
         }
     }
     return arr[num];
